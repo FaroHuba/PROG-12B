764ce13a958a82b5c0e18f1352955090aca80631
FaroHuba <152320763+FaroHuba@users.noreply.github.com>
2026-09-17 08:42:35 +0200

    Update githubproba.py

--- githubproba.py
+++ githubproba.py
@@ -5,3 +5,12 @@ osszeg=0
 for szam in szamok:
     osszeg+=szam
 print(f"Átlag: {osszeg/len(szamok)}")
+
+#van e paros szam
+i=0
+while i < len(szamok) and szamok[i] % 2!=0:
+    i+=1
+if i<len(szamok):
+    print("Van páros szám")
+else:
+    print("Nincs paros szam")
